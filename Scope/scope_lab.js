c2c8e0c98dd3ffc041ec4b7c3c2681209235a301
Aanchal <sharmaaanchal27@gmail.com>
2026-04-09 02:32:44 -0400

    git commit -m save changes x

---
+++ Scope/scope_lab.js
@@ -0,0 +1,36 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+    //Block scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let" ;
+    const blockConst ="I'm a block-scoped const";
+}
+
+// global scope
+
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst); 
+
+//block scope
+
+//console.log(blockVar);
+//console.log(blockLet);
+//console.log(blockConst);
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+
+}
+
+show();
+
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionConst);
